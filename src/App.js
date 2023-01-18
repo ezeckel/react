4971c18759899e8e1087b2d1b0d0e70032cf5f60
ezequiel viegas <117036591+ezeckel@users.noreply.github.com>
2023-01-18 20:23:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+const name = "ezequiel"
+
+
+
+  return (
+    <div className="App">
+
+    
+      <h1> ola {name} react </h1>
+      <h2> uaaaaaaaau</h2>
+      <p3> testezao </p3>
+    </div>
+  );
+}
+  
+export default App;
